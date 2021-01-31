9c216c9da7bcb5634b51665ee0af33abfddc93a2
Jon Sorbie <jon.sorbie@gmail.com>
2021-01-31 11:10:45 -0600

    Scene, terrain, mover scripts

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    
+     void LateUpdate()
+     {
+         transform.position = target.position;
+     }
+}
